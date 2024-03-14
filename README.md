c1d3533642b25ce69725a0944c5d66fc1a0c471a
jcates <jcates14@gmail.com>
2024-03-14 10:33:14 -0700

    adding foundation

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # crftBOT
+A quick proof of concept for a Discord bot integrated with DALL-E.
+
+## Notes
+This is not a very efficient implementation. Some things to consider:
+1. This implementation listens for every message event. A better approach would be to use Discord's [Slash Commands](https://discord.com/developers/docs/interactions/application-commands#slash-commands).
+2. There is no rate limiting.
+3. The bot responds with a link directly from OpenAI -- you may not want to expose this. 
+
+## Setup
+1. You'll need a Discord bot token and an OpenAI API key to run this. 
+2. Create a `config.json` file in your root folder to place the credentials.
+3. `npm install`
+4. Run the Node server to bring the bot online.
+
+### Example config.json
+```
+{
+	"token": "abc",
+	"oaiKey": "123"
+}
+```
+
+## Usage
+Once the bot is running, it will listen for messages that start with `!craft` and respond with a generated image from DALL-E. 
---
+++ index.js
@@ -0,0 +1,81 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
+const { token, oaiKey } = require('./config.json');
+const { OpenAI } = require('openai');
+// const fetch = require('node-fetch');
+
+
+// Create a new client instance
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.DirectMessages,
+		// GatewayIntentBits.GuildMembers,
+	],
+	partials: [
+		Partials.Message,
+		Partials.Channel,
+		Partials.Reaction,
+	],
+});
+
+
+// OpenAI API
+const openai = new OpenAI({
+	apiKey: oaiKey,
+});
+
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+
+// Hacky workaround for only allowing certain users to trigger the command and prompts based on Discord users.
+// message.author.username from the Discord object
+const allowedUsers = [
+	'bob',
+];
+
+
+// Listen for message events and do something
+client.on('messageCreate', async (message) => {
+	console.log(message.author.username + ': ' + message.content);
+	const parseCmd = message.content.substring(0, 6);
+
+	// Avoid infinite loops with the bot
+	if (message.author.bot) return;
+
+	if (allowedUsers.includes(message.author.username) && parseCmd === '!craft') {
+		const promptCapture = message.content.replace('!craft ', '');
+		console.log('Prompt: ' + promptCapture);
+
+		const response = await openai.images.generate({
+			model: 'dall-e-2',
+			prompt: promptCapture,
+			n: 1,
+			size: '1024x1024',
+		});
+
+		message.reply(response.data[0].url);
+	}
+
+	else if (!allowedUsers.includes(message.author.username) && parseCmd === '!craft') {
+		message.reply('You do not have permissions.');
+	}
+
+	else {
+		console.log('Doing nothing.');
+	}
+
+});
+
+
+// Log in to Discord with your client's token
+client.login(token);
